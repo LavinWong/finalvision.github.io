9a0b7695fa35c059a8a6bb5bf7eeaeadc21c054d
Zichong Wong <72814415+LavinWong@users.noreply.github.com>
2022-09-28 18:47:53 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
 //
 $(".navbar a").click(function(){
   $("body,html").animate({
-   scrollTop:$("#" + $(this).data('value')).offset().top-200
+   scrollTop:$("#" + $(this).data('value')).offset().top-100
   },1000)
  })
 
